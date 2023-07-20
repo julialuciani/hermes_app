2ff1177388dc74df4157e230ba272322287f7941
julialuciani <julia.luciani.oliveira@gmail.com>
2023-07-20 20:18:46 -0300

    adding _onOpen in initDb

--- assets/seeds/seeds.sql
+++
@@ -1,9 +0,0 @@
-INSERT OR IGNORE INTO `transaction_type` VALUES
-(1, 'income'),
-(2, 'expenses'),
-(3, 'investments');
-
---SPLIT--
-
-INSERT OR IGNORE INTO `category` VALUES
-(1, 'Alimentação', 2, 'food', 1, 234, 22, 22);
