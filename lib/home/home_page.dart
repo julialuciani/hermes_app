89cfe7b8c4b061e60e5972cbc7584f110f446a0d
julialuciani <julia.luciani.oliveira@gmail.com>
2023-07-27 20:29:31 -0300

    creating chart sketch

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,7 +10,87 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Container(),
+      body: const Chart(),
+    );
+  }
+}
+
+class Chart extends StatefulWidget {
+  const Chart({super.key});
+
+  @override
+  State<Chart> createState() => _ChartState();
+}
+
+class _ChartState extends State<Chart> {
+  int touchedIndex = -1;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        PieChart(
+          PieChartData(
+            centerSpaceRadius: 50,
+            pieTouchData: PieTouchData(
+              longPressDuration: const Duration(seconds: 200),
+              touchCallback: (event, pieTouchResponse) {
+                setState(() {
+                  if (!event.isInterestedForInteractions ||
+                      pieTouchResponse == null ||
+                      pieTouchResponse.touchedSection == null) {
+                    touchedIndex = -1;
+                    return;
+                  }
+                  touchedIndex =
+                      pieTouchResponse.touchedSection!.touchedSectionIndex;
+                });
+              },
+            ),
+            sections: [
+              PieChartSectionData(
+                value: 100,
+                color: Colors.blue,
+                radius: touchedIndex == 0 ? 50 : 20,
+                showTitle: touchedIndex == 0,
+                badgeWidget: touchedIndex == 0
+                    ? const CircleAvatar(child: Icon(Icons.celebration))
+                    : null,
+                badgePositionPercentageOffset: 0.9,
+                titlePositionPercentageOffset: 2,
+                title: "Gym",
+              ),
+              PieChartSectionData(
+                value: 100,
+                color: Colors.amber,
+                radius: touchedIndex == 1 ? 50 : 20,
+                showTitle: touchedIndex == 1,
+                badgeWidget: touchedIndex == 1
+                    ? const CircleAvatar(child: Icon(Icons.celebration))
+                    : null,
+                badgePositionPercentageOffset: 0.9,
+                title: "Gym",
+                titlePositionPercentageOffset: 2,
+              ),
+              PieChartSectionData(
+                value: 100,
+                color: Colors.red,
+                showTitle: touchedIndex == 2,
+                radius: touchedIndex == 2 ? 50 : 20,
+                badgeWidget: touchedIndex == 2
+                    ? const CircleAvatar(child: Icon(Icons.celebration))
+                    : null,
+                badgePositionPercentageOffset: 0.9,
+                title: "Gym",
+                titlePositionPercentageOffset: 2,
+              ),
+            ],
+          ),
+          swapAnimationCurve: Curves.easeIn,
+          swapAnimationDuration: const Duration(milliseconds: 10),
+        ),
+        const Text("Alooo")
+      ],
     );
   }
 }
