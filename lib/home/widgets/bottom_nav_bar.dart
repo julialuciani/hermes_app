5bb3bfa2fba770586588aa84555f8fe1f997b054
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2023-08-05 15:50:28 -0300

    feat: add bottom nav bar

---
+++ lib/home/widgets/bottom_nav_bar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:hermes_app/shared/theme/app_colors.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int currentIndex;
+
+  const BottomNavBar({
+    super.key,
+    required this.currentIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: const BoxDecoration(
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Colors.black54,
+              blurRadius: 15.0,
+              offset: Offset(0.0, 0.75))
+        ],
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      child: BottomNavigationBar(
+        currentIndex: currentIndex,
+        elevation: 30,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedItemColor: AppColors.black,
+        unselectedItemColor: AppColors.grey,
+        unselectedIconTheme: IconThemeData(color: AppColors.grey, size: 30),
+        selectedIconTheme: IconThemeData(color: AppColors.black, size: 30),
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: TextStyle(
+          color: AppColors.black,
+          fontWeight: FontWeight.bold,
+        ),
+        unselectedLabelStyle: TextStyle(
+          color: AppColors.grey,
+        ),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance),
+            label: "Balance",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            label: "Expenses",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.monetization_on),
+            label: "Income",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: "Investments",
+          ),
+        ],
+      ),
+    );
+  }
+}
