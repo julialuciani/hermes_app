0f6885f80cab6c7c2166b5e7b4f8b2ca86f9231f
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2023-11-28 19:00:11 -0300

    docs: add docs to content box widget

--- lib/shared/widgets/content_box/content_box.dart
+++ lib/shared/widgets/content_box/content_box.dart
@@ -3,10 +3,19 @@ import 'package:hermes_app/shared/extensions/build_context_extensions.dart';
 import 'package:hermes_app/shared/widgets/content_box/utils/content_box_mixin.dart';
 
 class ContentBox extends StatelessWidget with ContentBoxMixin {
+  /// [padding] the internal spacing that will exist within the component, if null, will apply the default, of 16
   final EdgeInsets? padding;
+
+  /// [child] the content that will be inside the component, which can be anything
   final Widget child;
+
+  /// [onTap] function that will be called when clicking on the box, if it is null, nothing will happen when clicking
   final VoidCallback? onTap;
+
+  /// [borderColor] allows customization of the component's border color, the default is [AppColors.black]
   final Color? borderColor;
+
+  /// [ContentBox] is a widget that must be used to group information that is related
   const ContentBox({
     super.key,
     this.padding,
