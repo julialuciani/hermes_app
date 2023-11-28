3ca419122ca7e3df3e2dccb05d3e60d33be239e1
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2023-11-28 18:56:57 -0300

    test: create tests to content box widget

--- lib/shared/widgets/content_box/content_box.dart
+++ lib/shared/widgets/content_box/content_box.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hermes_app/shared/extensions/build_context_extensions.dart';
-import 'package:hermes_app/shared/theme/app_colors.dart';
+import 'package:hermes_app/shared/widgets/content_box/utils/content_box_mixin.dart';
 
-class ContentBox extends StatelessWidget {
-  final EdgeInsets padding;
+class ContentBox extends StatelessWidget with ContentBoxMixin {
+  final EdgeInsets? padding;
   final Widget child;
   final VoidCallback? onTap;
   final Color? borderColor;
   const ContentBox({
     super.key,
-    this.padding = const EdgeInsets.all(16),
+    this.padding,
     required this.child,
     this.onTap,
     this.borderColor,
@@ -20,12 +20,12 @@ class ContentBox extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        padding: padding,
+        padding: getPadding(padding),
         width: context.screenWidth,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8),
           border: Border.all(
-            color: borderColor ?? AppColors.black,
+            color: getBorderColor(borderColor),
             width: 0.5,
           ),
         ),
