ab46c63ff7e4a8cb15eb2ae60622cf339d362690
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2023-11-29 20:55:27 -0300

    fix issues with database create

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:hermes_app/app_module.dart';
 import 'package:hermes_app/app_widget.dart';
+import 'package:hermes_app/shared/database/database_sqflite.dart';
 
-void main() {
-  runZonedGuarded(
-    () {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await runZonedGuarded(
+    () async {
+      await DatabaseSqflite.instance.database;
+
       runApp(
         ModularApp(
           module: AppModule(),
