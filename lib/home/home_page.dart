b4ffd115352861bc180158256d36edb50fec8c28
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2023-11-29 22:43:13 -0300

    feat: add docs

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hermes_app/shared/widgets/expansion_tile/expansion_tile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,24 +9,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: const [
-            ExpandableBox(
-              title: Text(
-                "06/07/2023",
-              ),
-              children: [
-                Text("BB"),
-                Text("BB"),
-                Text("BB"),
-                Text("BB"),
-              ],
-            ),
-          ],
-        ),
-      ),
+      body: const SizedBox(),
     );
   }
 }
--- lib/shared/widgets/expansion_tile/expansion_tile.dart
+++ lib/shared/widgets/expansion_tile/expansion_tile.dart
@@ -2,14 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:hermes_app/shared/theme/app_colors.dart';
 import 'package:hermes_app/shared/theme/app_typography.dart';
 
-class ExpandableBox extends StatelessWidget {
+class ExpandableBox extends StatefulWidget {
+  /// [title] a required Widget that will be displayed at the top of the [ExpandableBox]
   final Widget title;
+
+  /// [subtitle] a Widget that will be displayed below the title
+  final Widget? subtitle;
+
+  /// [children] a [List] of [Widget] that will fill the [ExpandableBox] when expanded
   final List<Widget> children;
+
+  /// [initiallyExpanded] is used to set an initial value to the [ExpandableBox]
+  final bool? initiallyExpanded;
+
+  /// [ExpandableBox] is a widget that contains an arrow and hides or displays the [children] according to the state
   const ExpandableBox({
-    super.key,
+    Key? key,
     required this.title,
+    this.subtitle,
     required this.children,
-  });
+    this.initiallyExpanded,
+  }) : super(key: key);
+
+  @override
+  State<ExpandableBox> createState() => _ExpandableBoxState();
+}
+
+class _ExpandableBoxState extends State<ExpandableBox> {
+  late bool isExpanded;
+
+  @override
+  void initState() {
+    isExpanded = widget.initiallyExpanded ?? false;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,11 +50,25 @@ class ExpandableBox extends StatelessWidget {
       clipBehavior: Clip.antiAlias,
       margin: EdgeInsets.zero,
       child: ExpansionTile(
+        onExpansionChanged: (val) => setState(() => isExpanded = val),
+        controlAffinity: ListTileControlAffinity.trailing,
+        childrenPadding: const EdgeInsets.all(8),
         title: DefaultTextStyle(
           style: typography.regular.medium,
-          child: title,
+          child: widget.title,
         ),
-        children: children,
+        subtitle: widget.subtitle != null
+            ? DefaultTextStyle(
+                style: typography.regular.small,
+                child: widget.subtitle!,
+              )
+            : null,
+        trailing: Icon(
+          isExpanded ? Icons.keyboard_arrow_up : Icons.keyboard_arrow_down,
+          color: AppColors.black,
+          size: 30,
+        ),
+        children: widget.children,
       ),
     );
   }
