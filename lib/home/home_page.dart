8d4da174af52069746f957601b913a6f70a386c5
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2024-01-23 22:03:58 -0300

    feat: ending implementation of component - tests missing

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,30 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:hermes_app/shared/widgets/default_button/default_button.dart';
 import 'package:hermes_app/shared/widgets/dropdown/dropdown.dart';
 import 'package:hermes_app/shared/widgets/dropdown/dropdown_item.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Dropdown(
-              items: List.generate(
-                5,
-                (index) => DropdownItem(
-                    description: "Item ${index + 1}", value: index),
+      body: Form(
+        key: formKey,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Dropdown(
+                items: List.generate(
+                  90,
+                  (index) => DropdownItem(
+                      description: "Item ${index + 1}", value: index),
+                ),
+                onChanged: (id) {},
+                label: "Teste",
+                validator: (value) {
+                  if (value == null) return "Valor precisa ser selecionado";
+                  if (value.isEven) return "Valor precisa ser par";
+                  return null;
+                },
+                enabled: true,
+                value: 2,
               ),
-              onChanged: (id) {},
             ),
-          ),
-        ],
+            DefaultButton(
+              onPressed: () {
+                formKey.currentState?.validate();
+              },
+              title: const Text("Send form"),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/shared/widgets/dropdown/dropdown.dart
+++ lib/shared/widgets/dropdown/dropdown.dart
@@ -1,35 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:hermes_app/shared/extensions/build_context_extensions.dart';
+import 'package:hermes_app/shared/theme/app_colors.dart';
 import 'package:hermes_app/shared/widgets/dropdown/dropdown_item.dart';
+import 'package:hermes_app/shared/widgets/input/utils/input_mixin.dart';
 
 class Dropdown extends StatefulWidget {
   final List<DropdownItem> items;
-  final void Function(int itemId) onChanged;
+  final void Function(int? itemId) onChanged;
   final int? value;
+  final String? Function(int? value)? validator;
+  final String? label;
+  final bool enabled;
   const Dropdown({
     super.key,
     required this.items,
     required this.onChanged,
     this.value,
+    this.validator,
+    this.label,
+    this.enabled = true,
   });
 
   @override
   State<Dropdown> createState() => _DropdownState();
 }
 
-class _DropdownState extends State<Dropdown> {
+class _DropdownState extends State<Dropdown> with InputMixin {
+  String? errorText;
+
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonFormField(
-      value: widget.value,
-      items: widget.items
-          .map((e) => DropdownMenuItem(
-                value: e.value,
-                onTap: () => widget.onChanged(e.value),
-                child: Text(e.description),
-              ))
-          .toList(),
-      onChanged: (id) => id != null ? widget.onChanged(id) : null,
-      decoration: const InputDecoration(),
+    final typography = context.typography;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (widget.label != null) ...[
+          Text(
+            widget.label!,
+            style: typography.regular.medium,
+          ),
+          const SizedBox(height: 6),
+        ],
+        DropdownButtonFormField<int>(
+          style: widget.enabled
+              ? typography.regular.medium
+              : typography.paragraph.medium,
+          value: widget.value,
+          items: [
+            const DropdownMenuItem(child: Text("Selecione...")),
+            ...widget.items.map((e) => DropdownMenuItem(
+                  value: e.value,
+                  onTap: () => widget.onChanged(e.value),
+                  child: Text(e.description),
+                )),
+          ],
+          validator: (value) {
+            final result = widget.validator?.call(value);
+
+            WidgetsBinding.instance.addPostFrameCallback(
+              (timeStamp) => setState(() => errorText = result),
+            );
+
+            return null;
+          },
+          onChanged: widget.enabled ? (id) => widget.onChanged(id) : null,
+          decoration: InputDecoration(
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+            filled: true,
+            border: OutlineInputBorder(
+              borderSide: getBorderSideByErrorText(errorText),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: getBorderSideByErrorText(errorText),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: getBorderSideByErrorText(errorText),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        if (errorText != null && widget.enabled) ...[
+          const SizedBox(height: 4),
+          Text(
+            errorText!,
+            style: typography.regular.small.copyWith(
+              color: AppColors.red,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ],
     );
   }
 }
