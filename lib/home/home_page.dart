a7a68ee9c0a2f5faee369e18e5db3fff3f767f81
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2024-01-24 19:43:29 -0300

    refactor: clean home page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hermes_app/shared/widgets/default_button/default_button.dart';
-import 'package:hermes_app/shared/widgets/dropdown/dropdown.dart';
-import 'package:hermes_app/shared/widgets/dropdown/dropdown_item.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,38 +15,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Dropdown(
-                items: List.generate(
-                  90,
-                  (index) => DropdownItem(
-                      description: "Item ${index + 1}", value: index),
-                ),
-                onChanged: (id) {},
-                label: "Teste",
-                validator: (value) {
-                  if (value == null) return "Valor precisa ser selecionado";
-                  if (value.isEven) return "Valor precisa ser par";
-                  return null;
-                },
-                enabled: true,
-                value: 2,
-              ),
-            ),
-            DefaultButton(
-              onPressed: () {
-                formKey.currentState?.validate();
-              },
-              title: const Text("Send form"),
-            ),
-          ],
-        ),
-      ),
+      body: const SizedBox(),
     );
   }
 }
