6eca24dfe5bf5d31793b99ff43728bf38cef4c8c
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2024-01-24 19:44:34 -0300

    home page to stateless widget again

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
