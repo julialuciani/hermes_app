9f2cf75098d4c181eadb2a70a3210a77da0b9087
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2024-02-15 23:09:01 -0300

    feat: add label to ignore unused field

--- lib/shared/repositories/transaction_repository.dart
+++ lib/shared/repositories/transaction_repository.dart
@@ -4,6 +4,7 @@ import 'package:hermes_app/shared/entities/transaction_model.dart';
 import 'package:hermes_app/shared/repositories/base_repository.dart';
 
 class TransactionRepository extends BaseRepository<TransactionModel> {
+  // ignore: unused_field
   final IDatabase _db;
 
   TransactionRepository(this._db) : super(_db, Tables.transaction);
