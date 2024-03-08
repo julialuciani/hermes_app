d8baddccb7cc3ae0185c67dd48cc180a2dddccc4
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2024-03-07 21:10:33 -0300

    setup menu item widget

--- lib/home/widgets/home_drawer.dart
+++ lib/home/widgets/home_drawer.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:hermes_app/home/widgets/menu_item.dart';
+import 'package:hermes_app/shared/extensions/build_context_extensions.dart';
+import 'package:hermes_app/shared/theme/app_colors.dart';
 
 class HomeDrawer extends StatelessWidget {
   const HomeDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final typography = context.typography;
     return SafeArea(
       child: Drawer(
-        child: Column(
-          children: const [
-            SizedBox(height: 80),
-          ],
+        backgroundColor: AppColors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40),
+              Text(
+                'Menu',
+                style: typography.bold.extraLarge,
+              ),
+              const SizedBox(height: 20),
+              MenuItem(
+                icon: Icons.mail,
+                text: 'Categoria',
+                onPressed: () {
+                  //Call navigation to categories listing page
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/home/widgets/menu_item.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:hermes_app/shared/extensions/build_context_extensions.dart';
+import 'package:hermes_app/shared/theme/app_colors.dart';
+
+class MenuItem extends StatefulWidget {
+  final IconData icon;
+  final String text;
+  final VoidCallback onPressed;
+
+  const MenuItem({
+    Key? key,
+    required this.icon,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  State<MenuItem> createState() => _MenuItemState();
+}
+
+class _MenuItemState extends State<MenuItem> {
+  bool _isPressed = false;
+
+  void _updateState(bool isPressed) {
+    setState(() {
+      _isPressed = isPressed;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final typography = context.typography;
+    Color getContainerColorByState() {
+      return _isPressed ? AppColors.lightBlue : AppColors.white;
+    }
+
+    Color getIconAndTextColorByState() {
+      return _isPressed ? AppColors.blue : AppColors.darkGrey;
+    }
+
+    return GestureDetector(
+      onTap: widget.onPressed,
+      onTapDown: (_) => _updateState(true),
+      onTapUp: (_) => _updateState(false),
+      onTapCancel: () => _updateState(false),
+      onLongPress: () => _updateState(true),
+      onLongPressEnd: (_) => _updateState(false),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+        decoration: BoxDecoration(
+          color: getContainerColorByState(),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          children: [
+            Icon(
+              widget.icon,
+              color: getIconAndTextColorByState(),
+            ),
+            const SizedBox(width: 16),
+            Text(
+              widget.text,
+              style: typography.bold.medium.copyWith(
+                color: getIconAndTextColorByState(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
