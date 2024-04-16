8dbaa16d3d2de41c74c71bdbd1ea619b555b3ea1
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2024-04-16 18:05:50 -0300

    add period group text in screen

--- lib/home/expenses/expenses_screen.dart
+++ lib/home/expenses/expenses_screen.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:hermes_app/home/expenses/expenses_cubit.dart';
 import 'package:hermes_app/home/expenses/filters/expenses_filters_button.dart';
 import 'package:hermes_app/home/expenses/filters/expenses_screen_filters_cubit.dart';
 import 'package:hermes_app/home/expenses/widgets/expenses_period_row.dart';
 import 'package:hermes_app/home/utils/fetch_movements_filters.dart';
+import 'package:hermes_app/home/utils/period_group_enum.dart';
+import 'package:hermes_app/shared/extensions/build_context_extensions.dart';
 import 'package:hermes_app/shared/theme/app_colors.dart';
 import 'package:hermes_app/shared/utils/event_bus.dart';
 
@@ -43,6 +46,8 @@ class _ExpensesScreenState extends State<ExpensesScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final typography = context.typography;
+
     return Scaffold(
       appBar: AppBar(
         actions: const [ExpensesFiltersButton()],
@@ -55,11 +60,48 @@ class _ExpensesScreenState extends State<ExpensesScreen> {
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20),
         child: Column(
-          children: const [
-            ExpensesPeriodRow(),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const ExpensesPeriodRow(),
+            const SizedBox(height: 40),
+            _ExtractPeriodText(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _ExtractPeriodText extends StatefulWidget {
+  @override
+  State<_ExtractPeriodText> createState() => _ExtractPeriodTextState();
+}
+
+class _ExtractPeriodTextState extends State<_ExtractPeriodText> {
+  final _expensesFiltersCubit = Modular.get<ExpensesScreenFiltersCubit>();
+
+  @override
+  Widget build(BuildContext context) {
+    final typography = context.typography;
+    return BlocBuilder<ExpensesScreenFiltersCubit, FetchMovementsFilters>(
+      bloc: _expensesFiltersCubit,
+      builder: (context, state) {
+        String getTextByPeriod() {
+          switch (state.periodGroup) {
+            case PeriodGroup.day:
+            case PeriodGroup.week:
+            case PeriodGroup.month:
+              return 'Extrato diário';
+            case PeriodGroup.year:
+              return 'Extrato mensal';
+          }
+        }
+
+        return Text(
+          getTextByPeriod(),
+          style: typography.bold.medium,
+        );
+      },
     );
   }
 }
